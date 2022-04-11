95a73c34bb5556cc6ca73f56965588e2240fb932
Farid <faridmarconi@gmail.com>
2022-04-11 17:41:54 -0300

    API async

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var API_URL = "https://api.github.com/users/";
 var USERNAME = "faridmarconi";
 
-window.onload = function() {
-    var xml = new XMLHttpRequest();
-    xml.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            var div = document.getElementById("conteudo");
-            div.innerHTML = xml.responseText;
-        }
-    }
-
-    xml.open("GET", API_URL + USERNAME, true);
-    xml.send();
+window.onload = async function() {
+    const response = await fetch(`${API_URL}${USERNAME}`);
+    const dados = await response.json();
+    const div = document.getElementById("conteudo");
+        div.innerHTML = JSON.stringify(dados);
 }
+
+    // var xml = new XMLHttpRequest();
+    // xml.onreadystatechange = function() {
+    //     if(this.readyState === 4 && this.status === 200) {
+    //         var div = document.getElementById("conteudo");
+    //         div.innerHTML = xml.responseText;
+    //     }
+    // }
+    // xml.open("GET", API_URL + USERNAME, true);
+    // xml.send();
